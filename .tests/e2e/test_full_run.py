76931df9d13c2a82278df9cf3850c5a202d38c91
Ulthran <ctbushman@gmail.com>
2024-03-29 15:41:09 -0400

    Update e2e test structure

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -1,25 +1,25 @@
-import os
 import pytest
 import shutil
 import subprocess as sp
 import sys
 import tempfile
+from pathlib import Path
 
 
 @pytest.fixture
 def setup():
-    temp_dir = tempfile.mkdtemp()
+    temp_dir = Path(tempfile.mkdtemp())
 
-    reads_fp = os.path.abspath(".tests/data/reads/")
-    hosts_fp = os.path.abspath(".tests/data/hosts/")
+    reads_fp = Path(".tests/data/reads/").resolve()
+    hosts_fp = Path(".tests/data/hosts/").resolve()
 
-    project_dir = os.path.join(temp_dir, "project/")
+    project_dir = temp_dir / "project/"
 
     sp.check_output(["sunbeam", "init", "--data_fp", reads_fp, project_dir])
 
-    config_fp = os.path.join(project_dir, "sunbeam_config.yml")
+    config_fp = project_dir / "sunbeam_config.yml"
 
-    mapping_fp = os.path.join(project_dir, "mapping.yml")
+    mapping_fp = project_dir / "mapping.yml"
 
     with open(mapping_fp, "w") as f:
         f.write("A: ['A702', 'A741', 'A745', 'A746', 'A747']\n")
@@ -56,28 +56,20 @@ def setup():
 
     yield temp_dir, project_dir
 
-    shutil.rmtree(temp_dir)
-
 
 @pytest.fixture
 def run_sunbeam(setup):
     temp_dir, project_dir = setup
+    output_fp = project_dir / "sunbeam_output"
+    log_fp = output_fp / "logs"
+    stats_fp = project_dir / "stats"
 
-    output_fp = os.path.join(project_dir, "sunbeam_output")
-
-    def write_logs():
-        if os.environ.get("CI", False):
-            shutil.copytree(os.path.join(output_fp, "logs/"), "logs/")
-            shutil.copytree(os.path.join(project_dir, "stats/"), "stats/")
-
+    # Run the test job
     try:
-        # Run the test job
         sp.check_output(
             [
                 "sunbeam",
                 "run",
-                "--conda-frontend",
-                "conda",
                 "--profile",
                 project_dir,
                 "all_coassemble",
@@ -86,12 +78,15 @@ def run_sunbeam(setup):
             ]
         )
     except sp.CalledProcessError as e:
-        write_logs()
+        shutil.copytree(log_fp, "logs/")
+        shutil.copytree(stats_fp, "stats/")
         sys.exit(e)
 
-    write_logs()
+    shutil.copytree(log_fp, "logs/")
+    shutil.copytree(stats_fp, "stats/")
 
-    benchmarks_fp = os.path.join(project_dir, "stats/")
+    output_fp = project_dir / "sunbeam_output"
+    benchmarks_fp = project_dir / "stats/"
 
     yield output_fp, benchmarks_fp
 
@@ -99,11 +94,11 @@ def run_sunbeam(setup):
 def test_full_run(run_sunbeam):
     output_fp, benchmarks_fp = run_sunbeam
 
-    A_fp = os.path.join(output_fp, "assembly/coassembly/A_final_contigs.fa")
-    B_fp = os.path.join(output_fp, "assembly/coassembly/B_final_contigs.fa")
-    Other_fp = os.path.join(output_fp, "assembly/coassembly/Other_final_contigs.fa")
+    assembly_fp = output_fp / "assembly" / "coassembly"
+    A_fp = assembly_fp / "A_final_contigs.fa"
+    B_fp = assembly_fp / "B_final_contigs.fa"
+    Other_fp = assembly_fp / "Other_final_contigs.fa"
 
-    # Check output
-    assert os.path.exists(A_fp)
-    assert os.path.exists(B_fp)
-    assert os.path.exists(Other_fp)
+    assert A_fp.exists()
+    assert B_fp.exists()
+    assert Other_fp.exists()
